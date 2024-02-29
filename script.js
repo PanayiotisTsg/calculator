5fd14c89cf9f102b6d79fdb97e4e14ae7cd324b3
Panayiotis Tsangarides <139330855+PanayiotisTsg@users.noreply.github.com>
2024-02-29 15:00:45 +0200

    Add basic calculator functionality
    Users are able to do calculations (one operation at a time).

--- script.js
+++ script.js
@@ -5,10 +5,44 @@ const methods = {
     '/': (a, b) => a / b
 }
 
-const firstNum = 5;
-const secondNum = 3;
-const operator = '+';
+let firstNum;
+let secondNum;
+let operator;
+let displayOperator;
+let result;
+
 
 function operate(numA, operator, numB) {
-    return methods[operator](numA, numB);
+    return methods[operator](+numA, +numB);
 }
+
+const mainDisplay = document.querySelector('.main-display');
+const numButtons = document.querySelectorAll('.num');
+numButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        mainDisplay.textContent += e.target.textContent;
+        firstNum = mainDisplay.textContent.split(displayOperator)[0];
+        secondNum = mainDisplay.textContent.split(displayOperator)[1];
+        console.log(`first num: ${firstNum}`);
+        console.log(`second num: ${secondNum}`);
+    });
+})
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        mainDisplay.textContent += e.target.textContent;
+        displayOperator = e.target.textContent;
+        operator = displayOperator === '×' ? '*' :
+            displayOperator === '÷' ? '/' :
+            displayOperator;
+        console.log(`operator: ${operator}`);
+    })
+})
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', (e) => {
+    result = operate(firstNum, operator, secondNum);
+    mainDisplay.textContent = result;
+    firstNum = result;
+})
